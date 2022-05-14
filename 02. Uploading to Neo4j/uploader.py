f39770b295ac989253bd2a6a8ac4e99943c87838
PratikGarai <pratikgarai0208@gmail.com>
2022-05-14 22:20:10 +0530

    creation and deletion of roles

--- 02. Uploading to Neo4j/uploader.py
+++ 02. Uploading to Neo4j/uploader.py
@@ -7,18 +7,63 @@ import json
 
 class Neo:
     def __init__(self, uri, user, password):
-        try : 
+        try:
             self.driver = GraphDatabase.driver(uri, auth=(user, password))
             print(Fore.GREEN+"Successfully connected to Neo4J"+Style.RESET_ALL)
-        except Exception as e :
+        except Exception as e:
             print(Fore.RED+"Error : {e}"+Style.RESET_ALL)
             raise Exception("Failed to connect to database")
+
+    def create_roles(self, roles, orgId):
+        try:
+            with self.driver.session() as session:
+                for role in roles:
+                    session.write_transaction(
+                        self._create_role, role, orgId
+                    )
+            print(Fore.GREEN+"Successfully created roles"+Style.RESET_ALL)
+        except Exception as e:
+            print(Fore.RED+f"Error : {e}"+Style.RESET_ALL)
+            raise Exception("DB Error")
+
+    @staticmethod
+    def _create_role(tx, role, orgId):
+        try : 
+            query = '''
+                    CREATE (r: ROLE {name : $role, orgId : $orgId})
+                '''
+            tx.run(query, role=role, orgId=orgId)
+        except Exception as e:
+            print(Fore.RED+f"Error : {e}"+Style.RESET_ALL)
+            raise Exception("DB Transaction Error")
     
+    def delete_roles(self, orgId):
+        try:
+            with self.driver.session() as session:
+                session.write_transaction(
+                    self._delete_roles, orgId
+                )
+            print(Fore.GREEN+"Successfully deleted roles"+Style.RESET_ALL)
+        except Exception as e:
+            print(Fore.RED+f"Error : {e}"+Style.RESET_ALL)
+            raise Exception("DB Error")
+
+    @staticmethod
+    def _delete_roles(tx, orgId):
+        try : 
+            query = '''
+                    MATCH (r: ROLE {orgId : $orgId}) DELETE r
+                '''
+            tx.run(query, orgId=orgId)
+        except Exception as e:
+            print(Fore.RED+f"Error : {e}"+Style.RESET_ALL)
+            raise Exception("DB Transaction Error")
+            
     def close(self):
         self.driver.close()
 
 
-def get_triples(fname: str) -> List[Tuple[str, str, str]]:
+def get_roles_and_triples(fname: str) -> Tuple[List, List[Tuple[str, str, str]]]:
     try:
         f = json.load(open(fname, "r"))
         rel_map = f["map"]
@@ -33,17 +78,18 @@ def get_triples(fname: str) -> List[Tuple[str, str, str]]:
                 )
             )
         print(Fore.GREEN + "Scanned json successfully" + Style.RESET_ALL)
-        return res
+        return list(roles.values()), res
     except Exception as e:
         print(Fore.RED + f"Error : {e}" + Style.RESET_ALL)
-        return []
+        return [], []
 
-def get_credentials(fname : str) -> Dict :
-    try : 
+
+def get_credentials(fname: str) -> Dict:
+    try:
         f = dict(json.load(open(fname, "r")))
         required_fields = ["uri", "user", "password"]
-        for r in required_fields : 
-            if not r in f : 
+        for r in required_fields:
+            if not r in f:
                 raise Exception(f"No feild \'{r}\' found in file")
         print(Fore.GREEN + "Scanned credentials successfully" + Style.RESET_ALL)
         return f
@@ -54,14 +100,17 @@ def get_credentials(fname : str) -> Dict :
 
 def main() -> None:
     try:
-        triples = get_triples("relationships.json")
+        roles, triples = get_roles_and_triples("relationships.json")
         credentials = get_credentials("secrets.json")
-        
         n = Neo(
-            credentials["uri"], 
-            credentials["user"], 
+            credentials["uri"],
+            credentials["user"],
             credentials["password"]
         )
+        n.create_roles(roles, 1)
+        n.create_roles(roles, 2)
+        # n.delete_roles(1)
+        # n.delete_roles(1)
         n.close()
         print(Fore.GREEN + "Created Graph in Aura DB successfully" + Style.RESET_ALL)
 
